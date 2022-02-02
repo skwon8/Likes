32320af61605dd9d0cd6a086da99e1142b95a0d2
skwon8 <saengsukwon@gmail.com>
2022-02-02 00:30:09 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+var likeCount = document.querySelector('#like-count')
+console.log(likeCount);
+
+function incrementCount(){
+    likeCount.innerText++;
+}
+
+var likeCount1 = document.querySelector('#like-count1')
+console.log(likeCount1);
+
+function incrementCount1(){
+    likeCount1.innerText++;
+}
+
+var likeCount2 = document.querySelector('#like-count2')
+console.log(likeCount2);
+
+function incrementCount2(){
+    likeCount2.innerText++;
+}
+
